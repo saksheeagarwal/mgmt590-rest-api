450df39fee852b3d8aade8ed730ef4e62ebe4654
Sakshee Agarwal <saksheeagarwal@pal-nat186-71-213.itap.purdue.edu>
2021-05-27 12:40:01 -0400

    Exception handling

--- answer.py
+++ answer.py
@@ -7,21 +7,20 @@ import json
 import time
 import os
 
-
+#Connecting to the database
 conn = sqlite3.connect('database.db')
-print ("Opened database successfully")
+#Creating tables
+conn.execute('CREATE TABLE IF NOT EXISTS answered (timestamp DATETIME, model TEXT, answer TEXT, question TEXT, context TEXT)')
+conn.execute('CREATE TABLE IF NOT EXISTS models (name TEXT, tokenizer TEXT, model TEXT)')
 
-
-conn.execute('CREATE TABLE answered (timestamp DATETIME, model TEXT, answer TEXT, question TEXT, context TEXT)')
-conn.execute('CREATE TABLE models (name TEXT, tokenizer TEXT, model TEXT)')
-
-print ("Table created successfully")
 # Create my flask app
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello World!</p>"
+
+# Define handler for /models path
 
 @app.route("/models", methods=['DELETE', 'GET', 'PUT'])
 def models():
@@ -40,15 +39,12 @@ def models():
                 result.append(out)
         return (jsonify(result))
 
-
     if request.method == 'PUT':
-        print('in put')
-        datar = json.loads(request.data)
+        datar = request.json
         name = datar['name']
         tokenizer = datar['tokenizer']
         model = datar['model']
         with sqlite3.connect("database.db") as con:
-            print("Connected")
             cur = con.cursor()
             cur.execute("INSERT INTO models (name,tokenizer,model) VALUES(?, ?, ?)", (name, tokenizer, model))
             con.commit()
@@ -65,7 +61,6 @@ def models():
         return (jsonify(result))
 
     if request.method == 'DELETE':
-        print("in delete")
         model = request.args.get('model')
         print(model)
         with sqlite3.connect("database.db") as con:
@@ -98,16 +93,16 @@ def answer():
             cur = con.cursor()
             cur.execute("SELECT * FROM models WHERE name=?", (model_name,))
             row = cur.fetchone()
-
-        # Import model
+            if row == None:
+                return("Model doesn't exist. Please insert the model first.")
+            # Import model
             hg_comp = pipeline('question-answering', model=row[2], tokenizer=row[1])
 
-        # Answer the question
+            # Answer the question
             answer = hg_comp({'question': data['question'], 'context': data['context']})['answer']
             timestamp = int(time.time())
             cur.execute("INSERT INTO answered (timestamp, model, answer, question, context ) VALUES(?, ?, ?, ?, ?)",
                         (timestamp, model_name, answer, data['question'], data['context'] ))
-            print("inserted")
             con.commit()
         # Create the response body.
         out = {
@@ -116,7 +111,6 @@ def answer():
             "answer": answer,
             "question": data['question'],
             "context": data['context']
-
         }
 
         return jsonify(out)
@@ -124,6 +118,8 @@ def answer():
     if request.method == 'GET':
         start = request.args.get('start')
         end = request.args.get('end')
+        if start==None or end == None:
+            return ("Please input start and end time")
         with sqlite3.connect("database.db") as con:
             cur = con.cursor()
             query = "SELECT * FROM answered where timestamp > ? and timestamp < ?"
@@ -149,9 +145,4 @@ def answer():
 # Run if running "python answer.py"
 if __name__ == '__main__':
     # Run our Flask app and start listening for requests!
-  app.run(host='0.0.0.0',port=int(os.environ.get("PORT",8080)), threaded=True)
-
-
-
-
-
+    app.run(host='0.0.0.0',port=int(os.environ.get("PORT",8080)), threaded=True)
